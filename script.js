77c2ab22df9a0f01035f86cd38aaa3099478dae6
Guillaume BULLET <bulletguillaume@gmail.com>
2025-01-23 17:01:39 +0100

    V0.0.03
    Ajouts :
    - SCROLL TOP BUTTON

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ document.querySelectorAll('.accordion-header').forEach(header => {
     });
 });
 
+
+///------------------------------------------------------------------------------///
+///--------------------------/// SCROLL TOP BUTTON ///--------------------------///
+///----------------------------------------------------------------------------///
+
+// Sélectionne le bouton "Retour en haut"
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+// Affiche/masque le bouton en fonction de la position de défilement
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) { // Affiche si l'utilisateur descend de 300px
+        scrollToTopButton.classList.add("show");
+    } else {
+        scrollToTopButton.classList.remove("show");
+    }
+});
+
+// Ajoute un événement au clic pour remonter en haut de la page
+scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Défilement fluide
+    });
+});

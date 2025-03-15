d7c752dee9849b143d310d816d81f7d9f0b2aecf
GuiB2776 <56394533+GuiB2776@users.noreply.github.com>
2025-03-15 23:53:17 +0100

    V0.0.18
    - Modification des noms des pages
    - Modification du menuNav
    - Ajout de la page : nommer-ses-pages-html.html

--- scripts/headerNavMobile.js
+++ scripts/headerNavMobile.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
         <nav id="mobile-nav">
             <ul class="mobile-navigation">
-                <li><a href="#"> À Propos De Moi</a>
+                <li><a href="#"> À Propos De Moi</a> <!-- Menu : A Propos -->
                     <ul>
                         <li><a href="index.html#apropos">À propos de moi</a></li>
                         <li><a href="index.html#formations">Formations</a>
@@ -30,32 +30,113 @@ document.addEventListener("DOMContentLoaded", function () {
                         <li><a href="index.html#contact">Me Contacter</a></li>
                     </ul>
                 </li>
-                <li><a href="#"> Marketing Digital </a>
+                <li><a href="#"> Marketing Digital </a> <!-- Menu : Marketing Digital -->
                     <ul>
-                        <li><a href="marketingDigital.html"> Qu'est ce que le Marketing Digital ?</a></li>
-                        <li><a href="simulateurSERP.html">Simulateur SERP</a></li>
-                        <li><a href="#">JavaScript</a>
+                        <li><a href="marketing-digital.html"> Qu'est ce que le Marketing Digital ?</a>
                             <ul>
-                                <li><a href="#">Ajax</a></li>
-                                <li><a href="#">jQuery</a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 1 : Introduction </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 2 : Création et Optimisation de la Présence en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 3 : Publicité en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 4 : Marketing de Contenu </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 5 : Email Marketing et Automatisation </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 6 : Réseaux Sociaux et Community Management </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 7 : Analyse de Données et ROI </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Chapitre 8 : Tendances et Innovations en Marketing Digital </a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"> Bonnes Pratiques & Conseils </a>
+                            <ul>
+                                <li><a href="nommer-ses-pages-html.html"> Nommer ses pages </a></li>
+                            </ul>
+                        </li>
+                        <li><a href="se-former-au-marketing-digital.html"> Apprendre le Marketing Digital </a>
+                            <ul>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 1 : Introduction </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 2 : Création et Optimisation de la Présence en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 3 : Publicité en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 4 : Marketing de Contenu </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 5 : Email Marketing et Automatisation </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 6 : Réseaux Sociaux et Community Management </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 7 : Analyse de Données et ROI </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 8 : Tendances et Innovations en Marketing Digital </a></li>
+                            </ul>
+                        </li>
+                        <li><a href="outils-de-marketing-digital.html"> Outils Marketing</a>
+                            <ul>
+                                <li><a href="simulateur-SERP.html"> Simulateur de SERP </a></li>
+                                <li><a href="generateur-utm.html"> Générateur d'URL UTM </a></li>
+                                <li><a href="exemple-de-qcm-personnalise.html">Exemple de QCM personnalisé </a></li>
                             </ul>
                         </li>
                     </ul>
                 </li>
-                <li><a href="#"> Projets </a>
+                <li><a href="#"> HTML - CSS - JavaScript </a> <!-- Menu : HTML - CSS - JavaScript -->
                     <ul>
-                        <li><a href="#">HTML</a></li>
+                        <li><a href="#">HTML</a>
+                            <ul>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 1 : Introduction </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 2 : Création et Optimisation de la Présence en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 3 : Publicité en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 4 : Marketing de Contenu </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 5 : Email Marketing et Automatisation </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 6 : Réseaux Sociaux et Community Management </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 7 : Analyse de Données et ROI </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 8 : Tendances et Innovations en Marketing Digital </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 9 : Tendances et Innovations en Marketing Digital </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 10 : Tendances et Innovations en Marketing Digital </a></li>
+                            </ul>
+                        </li>
                         <li><a href="#">CSS</a>
                             <ul>
-                                <li><a href="#">Resets</a></li>
-                                <li><a href="#">Grids</a></li>
-                                <li><a href="#">Frameworks</a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 1 : Introduction </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 2 : Création et Optimisation de la Présence en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 3 : Publicité en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 4 : Marketing de Contenu </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 5 : Email Marketing et Automatisation </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 6 : Réseaux Sociaux et Community Management </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 7 : Analyse de Données et ROI </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 8 : Tendances et Innovations en Marketing Digital </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 9 : Tendances et Innovations en Marketing Digital </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 10 : Tendances et Innovations en Marketing Digital </a></li>
                             </ul>
                         </li>
                         <li><a href="#">JavaScript</a>
                             <ul>
-                                <li><a href="#">Ajax</a></li>
-                                <li><a href="#">jQuery</a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 1 : Introduction </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 2 : Création et Optimisation de la Présence en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 3 : Publicité en Ligne </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 4 : Marketing de Contenu </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 5 : Email Marketing et Automatisation </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 6 : Réseaux Sociaux et Community Management </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 7 : Analyse de Données et ROI </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 8 : Tendances et Innovations en Marketing Digital </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 9 : Tendances et Innovations en Marketing Digital </a></li>
+                                <li><a href="introduction-au-marketing-digital.html"> Module 10 : Tendances et Innovations en Marketing Digital </a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#"> Exemples </a> <!-- Menu : Projets -->
+                    <ul>
+                        <li><a href="#"> Sites : HTML CSS JavaScript </a>
+                            <ul>
+                                <li><a href="#"> Template 1 </a></li>
+                                <li><a href="#"> Template 2 </a></li>
+                                <li><a href="#"> Template 3 </a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#"> CMS & CRM</a>
+                            <ul>
+                                <li><a href="#">WordPress</a></li>
+                                <li><a href="#">Grids</a></li>
+                                <li><a href="#">Frameworks</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"> IA et OSINT </a>
+                            <ul>
+                                <li><a href="#"> Chat GPT </a></li>
+                                <li><a href="#"> OSINT </a></li>
                             </ul>
                         </li>
                     </ul>
